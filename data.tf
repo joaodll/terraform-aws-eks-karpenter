bddb59800ce983765603f0130f0720a3a0b2cdd1
João Dalla Rosa <joao.custodiodllrosa@gmail.com>
2026-01-20 20:48:22 -0600

    feat: move locals to data file and add network datas

---
+++ data.tf
@@ -0,0 +1,23 @@
+data "aws_vpc" "vpc" {
+  filter {
+    name   = "tag:EKS_VPC"
+    values = ["true"]
+  }
+}
+
+data "aws_subnets" "pvt_eks_subnets" {
+  tags = {
+    Subnet_type = "private"
+    EKS_VPC     = "true"
+  }
+}
+
+
+locals {
+  karpenter_node_name = "eks-karpenter-pods"
+  cluster_name        = "my-acme-cluster"
+
+  tags = {
+    Project = "ACME"
+  }
+}
--- locals.tf
+++
@@ -1,7 +0,0 @@
-locals {
-  karpenter_node_name = "eks-karpenter-pods"
-  cluster_name        = "my-acme-cluster"
-  tags = {
-    Project = "ACME"
-  }
-}
